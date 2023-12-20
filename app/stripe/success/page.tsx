4c86e8a089045f940cf83f8c2f53aeedef99525a
Eduardo Arce <arce@nevada.unr.edu>
2023-12-19 21:11:42 -0800

    more changes in the delay after payment. Not working

--- app/stripe/success/page.tsx
+++ app/stripe/success/page.tsx
@@ -1,6 +1,9 @@
 import { CheckCheck } from 'lucide-react';
 import Link from 'next/link';
-
+import { Navigate } from "react-router-dom";
+function timeout(delay: number) {
+    return new Promise(res => setTimeout(res, delay));
+}
 export default function StripeSuccess() {
   return (
     <div className="h-screen">
@@ -11,10 +14,15 @@ export default function StripeSuccess() {
         </div>
         <div className="flex justify-center">
           <button className="bg-stone-800 text-slate-100 rounded-2xl py-2 px-6 hover:opacity-75 transition-opacity mt-6">
-            <Link href="/">Go Home</Link>
+                      <Link href="/">Go Home</Link>
+
           </button>
         </div>
       </div>
     </div>
   );
 }
+<>
+await timeout(5000);
+    <Link to="/Go Home">Go Home</Link>
+    </>
