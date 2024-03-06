df1f7443c5edcc8ba25ad12494f0c4c2ede6a5cc
Vito-Ethan <ethanvito@nevada.unr.edu>
2024-03-05 16:18:56 -0800

    Add example data to page

--- app/women/page.tsx
+++ app/women/page.tsx
@@ -1,5 +1,23 @@
 import CategoryPage from '../_components/category-page';
+import { Product } from '../interface';
+import { client } from '../lib/santiy';
+async function getData() {
+  // get the 4 most most recently added products
+  const query = `*[_type == "product"][0...4] | order(_createdAt desc){
+    _id,
+      price,
+      name,
+      "slug": slug.current,
+      "categoryName": category->name,
+      "imageUrl": images[0].asset->url
+  }`;
 
-export default function Women() {
-  return <CategoryPage catName="Women" />;
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export default async function Women() {
+  const data: Product[] = await getData();
+  return <CategoryPage catName="Women" data={data} />;
 }
