50dec6ee217356757e4709e17684e461222b0bfb
Vito-Ethan <ethanvito@nevada.unr.edu>
2024-04-14 16:29:10 -0700

    Add sanity db helper functions

--- app/lib/santiy.ts
+++ app/lib/santiy.ts
@@ -9,6 +9,15 @@ export const client = createClient({
   useCdn: true,
 });
 
+//client instance that can write to documents
+export const writeClient = createClient({
+  projectId: '6uopnzeq',
+  dataset: 'production',
+  apiVersion: '2022-03-25',
+  useCdn: true,
+  token: process.env.SANITY_TOKEN,
+});
+
 const builder = imageUrlBuilder(client);
 
 export function urlFor(source: any) {
