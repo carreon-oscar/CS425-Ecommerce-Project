b0b566fcb080ea066c68f31a2c5c32b2075c6cd6
Oscar <ocarreonnegreros@nevada.unr.edu>
2024-04-16 14:21:31 -0700

    Added subcategory schema

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,5 +1,6 @@
 import category from './category'
 import heroBanners from './heroBanners'
 import product from './product'
+import subcategory from './subcategory'
 
-export const schemaTypes = [product, category, heroBanners]
+export const schemaTypes = [product, category, subcategory, heroBanners]
