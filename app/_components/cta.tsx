55d7ffe0bd94a6837f2e297fe56c2da9daed41a0
Vito-Ethan <ethanvito@nevada.unr.edu>
2024-04-21 17:05:06 -0700

    Fix linter error

--- app/_components/cta.tsx
+++ app/_components/cta.tsx
@@ -10,12 +10,12 @@ export default function CTA() {
         <div className="flex justify-center p-6">
           <Link href="men/men-t-shirts">
             <div className="bg-stone-800 text-slate-100 rounded-2xl py-1 px-4 mr-4 hover:cursor-pointer hover:opacity-80">
-              Men's T-Shirts
+              {`Men's T-Shirts`}
             </div>
           </Link>
           <Link href="women/women-t-shirts">
             <div className="bg-stone-800 text-slate-100 rounded-2xl py-1 px-4 hover:cursor-pointer hover:opacity-80">
-              Women's T-Shirts
+              {`Women's T-Shirts`}
             </div>
           </Link>
         </div>
